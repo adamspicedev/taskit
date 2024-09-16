32dbacd95619767f929033ed988e40ff42df3a50
Adam Spice <adam@spicey.dev>
2024-09-17 11:12:49 +1200

    moved images to s3

--- client/src/components/BoardView/index.tsx
+++ client/src/components/BoardView/index.tsx
@@ -176,7 +176,7 @@ const Task = ({ task }: TaskProps) => {
     >
       {task.attachments && task.attachments.length > 0 && (
         <Image
-          src={`/${task.attachments[0].fileURL}`}
+          src={`${[process.env.NEXT_PUBLIC_IMAGE_URL]}/${task.attachments[0].fileURL}`}
           alt={task.attachments[0].fileName}
           width={400}
           height={200}
@@ -227,7 +227,7 @@ const Task = ({ task }: TaskProps) => {
             {task.assignee && (
               <Image
                 key={task.assignee.userId}
-                src={`/${task.assignee.profilePictureUrl}`}
+                src={`${[process.env.NEXT_PUBLIC_IMAGE_URL]}/${task.assignee.profilePictureUrl}`}
                 alt={task.assignee.username}
                 width={30}
                 height={30}
@@ -237,7 +237,7 @@ const Task = ({ task }: TaskProps) => {
             {task.author && (
               <Image
                 key={task.author.userId}
-                src={`/${task.author.profilePictureUrl}`}
+                src={`${[process.env.NEXT_PUBLIC_IMAGE_URL]}/${task.author.profilePictureUrl}`}
                 alt={task.author.username}
                 width={30}
                 height={30}
--- client/src/components/UserCard/index.tsx
+++ client/src/components/UserCard/index.tsx
@@ -11,7 +11,7 @@ const UserCard = ({ user }: Props) => {
     <div className="flex items-center rounded border p-4 shadow">
       {user.profilePictureUrl && (
         <Image
-          src={`https://pm-s3-images.s3.us-east-2.amazonaws.com/p1.jpeg`}
+          src={`${[process.env.NEXT_PUBLIC_IMAGE_URL]}/p1.jpeg`}
           alt="profile picture"
           width={32}
           height={32}
